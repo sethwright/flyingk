eedd8aa923b6569a44df14959312fac2a421da95
Seth Wright <sethwright@users.noreply.github.com>
2021-05-24 14:44:02 +0900

    polish readme

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import * as VueGoogleMaps from "vue2-google-maps";
 import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import dotenv from "dotenv";
 
+dotenv.config();
+console.log("VUE_APP_MAP_KEY");
+console.log(process.env.VUE_APP_SOMEKEY);
+// hi
 Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
@@ -18,3 +23,5 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+// VUE_APP_MAP_KEY
